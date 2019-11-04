0dfcff99ed646bbdb89706b57e7cb01c04975d20
Cosmin Harangus <cosmin@around25.com>
2019-11-04 22:00:00 +0200

    Fix saving market as backup
    Former-commit-id: ea2ef408e771fe26a051c401f40663449dd4f030

--- server/market_engine_backup.go
+++ server/market_engine_backup.go
@@ -46,7 +46,6 @@ func (mkt *marketEngine) LoadMarketFromBackup() (err error) {
 		Int("limit_sell_count", len(market.GetSellOrders())).
 		Msg("Loading market from backup")
 	mkt.LoadMarket(market)
-
 	// mark the last message that has been processed by the engine to the one saved in the backup file
 	err = mkt.consumer.SetOffset(offset)
 	if err != nil {
